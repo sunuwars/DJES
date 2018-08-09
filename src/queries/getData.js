4d9ff0bfdaff067470f3eae9d3c80ea62d8580ef
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-08-09 18:19:23 +0100

    request to borrow function implement

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -3,7 +3,7 @@ const databaseConnection = require("../database/db_connection.js");
 // qry is the db query we want to make
 const getData = (qry, cb) => {
   databaseConnection.query(
-    `SELECT users.name, users.fav_colour, items.name, items.description 
+    `SELECT users.name, users.fav_colour, items.name, items.id, items.description 
     FROM items 
     INNER JOIN users 
     ON items.lender_id = users.id 
