33f8dd0d1bfa7ee55c93ad3e56e9fc5c2cc68c11
Joe Friel <joseph.s.friel@gmail.com>
2018-08-09 10:09:41 +0100

    Fix eslint config and gitignore config

--- src/router.js
+++ src/router.js
@@ -1,27 +1,19 @@
-const http = require('http');
-const fs = require('fs');
-const handlers = require('./handlers');
-
-// check why we've put this here
-const pg = require('pg');
-
-const queryString = require('querystring');
+const handlers = require("./handlers");
 
 const router = (req, res) => {
-  const endpoint = req.url.split('/')[1];
+  const endpoint = req.url.split("/")[1];
 
-  if (endpoint === '') {
+  if (endpoint === "") {
     handlers.home(req, res);
-  } else if (endpoint === 'search') {
+  } else if (endpoint === "search") {
     handlers.search(req, res); // some arguments maybe
-  } else if (endpoint === 'request-item') {
+  } else if (endpoint === "request-item") {
     handlers.requestItem(); // some arguments
-  } else if (endpoint === 'add-item') {
+  } else if (endpoint === "add-item") {
     handlers.addItem(); // some arguments
   } else {
     handlers.public(req, res, endpoint);
   }
-
-}
+};
 
 module.exports = router;
