e1a49c4dc9b14702ae2541deba6860966c71e37c
Dominic Coelho <domwork@live.com>
2018-08-09 10:39:57 +0100

    declared contentType and prettier formatted handlers.js

--- src/handlers.js
+++ src/handlers.js
@@ -1,12 +1,12 @@
-const superRequest = require('request');
-const fs = require('fs');
-const path = require('path');
+const superRequest = require("request");
+const fs = require("fs");
+const path = require("path");
 
-const buildPath = function (myPath) {
+const buildPath = function(myPath) {
   return path.join(__dirname, "..", "public", myPath);
 };
 
-contentType = {
+const contentType = {
   ".html": "text/html",
   ".css": "text/css",
   ".js": "application/javascript",
@@ -15,51 +15,48 @@ contentType = {
   ".jpeg": "image/jpeg",
   ".png": "image/png",
   ".gif": "image/gif"
-}
+};
 
 const handlers = {
-
-  home: function (req, res) {
-    fs.readFile(buildPath('index.html'), (err, file) => {
+  home: function(req, res) {
+    fs.readFile(buildPath("index.html"), (err, file) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/html' });
-        res.end('<h1>Server Error</h1>');
-        console.log("home error")
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("<h1>Server Error</h1>");
+        console.log("home error");
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.end(file);
       }
-    })
+    });
   },
 
-  public: function (req, res, endpoint) {
+  public: function(req, res, endpoint) {
     fs.readFile(buildPath(endpoint), (err, file) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/html' });
-        res.end('<h1>Server Error</h1>');
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("<h1>Server Error</h1>");
         console.log("public error");
       } else {
         // const ext = endpoint.split('.')[1];
         console.log("ext: ", path.extname(endpoint));
-        res.writeHead(200, { 'Content-Type': contentType[path.extname(endpoint)] });
+        res.writeHead(200, {
+          "Content-Type": contentType[path.extname(endpoint)]
+        });
         res.end(file);
       }
-    })
+    });
     // add some innards
   },
-  search: function (req, res) {
-    // add some innards
-
-
-
-  },
-  requestItem: function () {
+  search: function(req, res) {
     // add some innards
   },
-  addItem: function () {
+  requestItem: function() {
+    // add some innards
+  },
+  addItem: function() {
     // add some innards
   }
-
-}
+};
 
 module.exports = handlers;
