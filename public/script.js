33112d1420f074b61d40afce43f9635c3871f73d
Dominic Coelho <domwork@live.com>
2018-08-10 13:15:38 +0100

    very basic colour change functionality

--- public/script.js
+++ public/script.js
@@ -58,7 +58,33 @@ submitItemBtn.addEventListener("click", function(e){
     favColour: favColour.replace("#","")
   };
   xhrPost.send(JSON.stringify(postData));
+
+  // this function will update the page style!
+  updateStyle(favColour);
+
 });
+
+function updateStyle(hex) {
+  var css = document.styleSheets[0];
+  var RGB = hexToRgb(hex);
+  var baseHSL = rgbToHsl(RGB.r, RGB.g, RGB.b);
+  var modifiedHSL = [baseHSL[0] + 10, baseHSL[1], baseHSL[2]]
+  console.log("RGB: ", RGB);
+  console.log("baseHSL: ", baseHSL);
+  console.log("modifiedHSL: ", modifiedHSL);
+
+  // hsl(10, 100%, 50%)  
+
+  console.log()
+
+  css.insertRule("h1{color:hsl(" + baseHSL.join(",") + ")}", document.styleSheets[0].cssRules.length);
+
+  css.insertRule("body{background-color:hsl(" + modifiedHSL.join(",") + ") }", document.styleSheets[0].cssRules.length)
+
+  // convert hex to hsl
+  // compute set of hsl values for style
+  // update element styles to reflect new style
+}
 
 requestBtn.addEventListener("click", function(e) {
   e.preventDefault();
@@ -159,3 +185,43 @@ function updateDom(err, data) {
 }
 
 request("/testing", 'GET', updateDom);
+
+// Function to convert hex to rgb. Nicked by Dom from ? on Stackoverflow
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+  } : null;
+}
+
+
+/* Function to convert rgb-to-hsl. Nicked by Dom from Pankaj Parashar on codepen */
+function rgbToHsl(r, g, b){
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) { h = s = 0; } 
+  else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max){
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    
+    h /= 6;
+  }
+  
+  return [(h*100+0.5)|0, ((s*100+0.5)|0) + '%', ((l*100+0.5)|0) + '%'];
+};
